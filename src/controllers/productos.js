3329dda961c97efaf2baf529f0de8ec4d0c9c8de
lukeehirsch <lucashirschdav@gmail.com>
2021-07-11 17:08:34 -0300

    styles usuario

--- src/controllers/productos.js
+++ src/controllers/productos.js
@@ -1,4 +1,4 @@
 module.exports = {
-    index: (req, res) => res.render("products/list"),
-    show: (req, res) => res.render("products/detail"),
+    index: (req, res) => res.render("products/list", {title: "Listado de productos", style: null }),
+    show: (req, res) => res.render("products/detail", {title: "Detalles de producto", style: null }),
 }
--- src/controllers/usuario.js
+++ src/controllers/usuario.js
@@ -1,4 +1,4 @@
 module.exports = {
-    login: (req, res) => res.render("users/login"),
-    register: (req, res) => res.render("users/register")
+    login: (req, res) => res.render("users/login", {title: "Formulario de login", style: "formlogin.css" }),
+    register: (req, res) => res.render("users/register", {title: "Formulario de Registro", style: "formregistro.css" })
 }
