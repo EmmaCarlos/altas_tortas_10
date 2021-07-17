b7bf402b7811f12d968025fbdc1d3eb2e97df4f1
christianpazos <christianlpazos@gmail.com>
2021-07-17 03:32:09 -0300

    christianpazos
    cambio segir desaroyando proyecto subido sprint 3 dia 17-07-2021 a las 03:31 hs
    
    Co-Authored-By: lukeehirsch <84397403+lukeehirsch@users.noreply.github.com>
    Co-Authored-By: VictoriaRoust <84426854+VictoriaRoust@users.noreply.github.com>

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,6 +1,15 @@
 module.exports = {
     index: (req, res) => res.render("home", {title: "Home", style: null }),
     contact:(req, res) => res.render("contact",{title: "Contacto"}),
-/*     search:(req, res) => res.render("products/list"), */
-
+     search:(req, res) => res.render("products/list"), 
+     
 }
+
+/*  consulta para el profesor    
+
+
+
+
+
+
+*/ 
--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,8 +1,10 @@
 const express = require ('express');
+const productos = require('../controllers/productos');
 const router = express.Router();
 const main = require ('../controllers/productos')
 
 router.get("/list/:category?", productos.index)
 router.get("/detail/:id", productos.show)
+router.get("/detail/:id/:nombre?,",productos.show)
 
 module.exports = router
