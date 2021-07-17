e54270ef961c13e2c237855fb70dcc64f31b4658
lukeehirsch <lucashirschdav@gmail.com>
2021-07-17 13:01:50 -0300

    pagina creacion faltan controladores

--- src/controllers/productos.js
+++ src/controllers/productos.js
@@ -2,6 +2,16 @@ const product = require ("../models/product")
 module.exports = {
     index: (req, res) => res.render("products/list", {title: "Listado de productos", style: list, products: product.allWithExtra() }),
     show: (req, res) => res.render("products/detail", {title: "Detalles de producto", style: null }),
-    create: (req, res) => res.render("products/create", {title: "Crear producto", style: null }),
+    create: (req, res) => res.render("products/create",{title: "Crear producto", style: formregistro,// store: ???
+  }),
     edit: (req, res) => res.render("products/edit", {title: "Edicion de producto", style: null }),
 }
+
+// store: (req, res) => {
+
+//     let group = req.body;
+
+//     groupId = groupsModel.create(group);
+
+//     res.redirect('/groups/' + groupId);
+// },
--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,10 +1,29 @@
 const express = require ('express');
 const productos = require('../controllers/productos');
+const multer = require('multer');
 const router = express.Router();
 const main = require ('../controllers/productos')
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../public/img/uploads'));
+    },
+    filename: (req, file, cb) => {
+        console.log(file);
+        const newFilename = 'torta-' + Date.now() + path.extname(file.originalname);
+        cb(null, newFilename);
+    }
+});
+
+const upload = multer({ storage });
 
 router.get("/list/:category?", productos.index)
 router.get("/detail/:id", productos.show)
 router.get("/detail/:id/:nombre?,",productos.show)
+router.get("/create", controller.create);
+
+
+
+router.post('/', upload.single('imagen'), controller.store);
 
 module.exports = router
