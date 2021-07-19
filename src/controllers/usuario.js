9ad2ffddeed6505262eea789621ecdf49c913099
lukeehirsch <lucashirschdav@gmail.com>
2021-07-19 19:38:49 -0300

    corrijo estilos de usuarios login y registro

--- src/controllers/usuario.js
+++ src/controllers/usuario.js
@@ -1,7 +1,7 @@
 /* require modelo de usuarios */
 module.exports = {
-    login: (req, res) => res.render("users/login", {title: "Formulario de login", style: "formlogin.css" }),
-    register: (req, res) => res.render("users/register", {title: "Formulario de Registro", style: "formregistro.css" })
+    login: (req, res) => res.render("users/login", {title: "Formulario de login", style: "formlogin" }),
+    register: (req, res) => res.render("users/register", {title: "Formulario de Registro", style: "formregistro" })
     /* access: post extra - utilizar el metodo de validPassword y el metodo byEmail - guardar el usuario logeado en sesion - instalar express sesion*/
     /*save: post register - utilizar metodo create del usuario*/
 }
