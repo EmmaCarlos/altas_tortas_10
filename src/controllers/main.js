a98f6181cd02055775ce8d3c911dc3773aa3723a
christianpazos <christianlpazos@gmail.com>
2021-07-20 17:02:35 -0300

    home para ver 4 productos alatorios

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,11 +1,22 @@
 const product = require ("../models/product")
 module.exports = {
-    index: (req, res) => res.render("home", {title: "Home", style: null }),
+    index: (req, res) => res.render("home",
+    {title: "home",
+    style: null,
+    productos:  product.randomProducs() }),
+ 
+    //index: (req, res) => res.render("home", {title: "Home", style: null }),
     contact:(req, res) => res.render("contact",{title: "Contacto"}),
     search:(req, res) => res.render("products/list"), 
-     
-}
+ 
+
+
+
+/*    
+}*/
   
 /* modificar el metodo index para enviar a la vista del home una array de 4 productos de forma alatoria  */
 
 /*  crear en el modelo de productos  un metodo para selecionar 4 productos de forma alatoria */ 
+
+}
